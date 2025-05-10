0cbeb969b884fe309234a38daecbb5c63bb72d03
Jessica Do <jessicanhu.do@gmail.com>
2025-05-09 22:43:22 -0500

    1.0 Initial Commit 5/9

---
+++ backend/controllers/tripController.js
@@ -0,0 +1,57 @@
+// backend/controllers/tripController.js
+const Trip = require("../models/Trip");
+
+const getTrips = async (req, res) => {
+  try {
+    const trips = await Trip.find();
+    res.json(trips);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const createTrip = async (req, res) => {
+  const { title, completed } = req.body;
+  const trip = new Trip({ title, completed });
+
+  try {
+    const savedTrip = await trip.save();
+    res.status(201).json(savedTrip);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+const updateTrip = async (req, res) => {
+  const { id } = req.params;
+  const { title, completed } = req.body;
+
+  try {
+    const updatedTrip = await Trip.findByIdAndUpdate(
+      id,
+      { title, completed },
+      { new: true }
+    );
+    res.json(updatedTrip);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+const deleteTrip = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Trip.findByIdAndDelete(id);
+    res.json({ message: "Trip deleted" });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+module.exports = {
+  getTrips,
+  createTrip,
+  updateTrip,
+  deleteTrip,
+};
---
+++ backend/routes/tripRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getTrip,
+  createTrip,
+  updateTrip,
+  deleteTrip,
+} = require("../controllers/tripController");
+
+// @route GET all /api/trips
+router.get("/", getTrip);
+// @route POST /api/trips
+router.post("/", createTrip);
+// @route PUT /api/trips/:id
+router.put("/:id", updateTrip);
+// @route DELETE /api/trips/:id
+router.put("/:id", deleteTrip);
+
+module.exports = router;
